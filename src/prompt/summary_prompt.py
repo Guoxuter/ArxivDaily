92661a2805df260e6d6b8fb23958b33e29d9eefa
guoxuter <guoxuter@gmail.com>
2025-10-30 11:52:28 +0800

    fix: 改掉输出格式问题

--- src/prompt/summary_prompt.py
+++ src/prompt/summary_prompt.py
@@ -6,21 +6,36 @@ summary_prompt = """你是一个论文摘要大师，你需要对所有论文进
 3. 以Markdown格式输出你的论文
 
 ## 输出格式
+1. 请你按照Markdown格式输出你的论文总结
+2. 保证排版符合 标准的 Markdown 格式
+3. 要求排版美观
+4. 需要用 ```markdown 和 ``` 包裹你的输出
+5. 直接输出论文的总结，不许输出其他内容
+
+```markdown
+---
+
+### 1 Paper Name
+
+**link**: https://arxiv.org/pdf/2409.04421
+**date**: 2024-09-04
+**keywords**: xxx
+**abs**: xxx_abs需要为中文
 
 ---
-### 1 Paper Name
-**link**: https://arxiv.org/pdf/2409.04421
-**date**: 2024-09-04
-**keywords**: xxx
-**abs**: xxx_abs需要为中文
----
+
 ### 2 Paper Name
+
 **link**: https://arxiv.org/pdf/2409.04421
 **date**: 2024-09-04
 **keywords**: xxx
 **abs**: xxx_abs需要为中文
+
 ---
+
 ...
+
+```
 
 ## 输入
 论文集合：{{input}}
